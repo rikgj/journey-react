309c1988e8d07d31533b30734670e651844b2b4e
rikgj <44588337+rikgj@users.noreply.github.com>
2024-11-20 15:31:44 +0100

    Endret en ting

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -8,7 +8,7 @@ export const ErrorPage = () => {
             </header>
             <main>
                 <h1>Page Not Found</h1>
-                <p>Sorry, the page you are looking for was not found 👷‍♂️</p>
+                <p>👷‍️ Sorry, the page you are looking for was not found 👷‍♂️</p>
             </main>
         </>
     );
