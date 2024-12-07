e56466aac1ba868645ca0d14f4d47a75ad877ed5
rikgj <44588337+rikgj@users.noreply.github.com>
2024-12-07 15:56:15 +0100

    WIP visning av flere bilder

--- src/pages/gallery/FramedPhoto.tsx
+++ src/pages/gallery/FramedPhoto.tsx
@@ -1,29 +1,18 @@
 import styles from "./Gallery.module.scss";
-import {useEffect, useState} from "react";
 
 
-export function FramedPhoto() {
-// take start ref as parameter
-    // must fetch css and js separately
-    // const url = "https://raw.githubusercontent.com/rikgj/journey-react/refs/heads/main/src/pages/Gallery.tsx"
-    const url = "/photos/photo-1.html";
+type FramedPjotoProps = {
+    srcDoc: string;
+}
 
-    const [data, setData] = useState("NO DATA");
-    useEffect(() => {
-        fetch(url)
-            .then(response => response.text())
-            .then(res => {
-                console.log(res)
-                setData(res)
-            })
-    }, []);
+export function FramedPhoto(props: FramedPjotoProps) {
     return (
         <>
             <div className={styles.frame}>
-                <div className={`${styles.framedPhotoInfo} ${styles.left} ${styles.code}`}>{data}</div>
-                <div className={`${styles.framedPhotoInfo} ${styles.right} ${styles.code}`}>{data}</div>
+                <div className={`${styles.framedPhotoInfo} ${styles.left} ${styles.code}`}>{props.srcDoc}</div>
+                <div className={`${styles.framedPhotoInfo} ${styles.right} ${styles.code}`}>{props.srcDoc}</div>
                 <div className={styles.framedPhoto}>
-                    <iframe srcDoc={data} sandbox="al"  />
+                    <iframe src={props.srcDoc} sandbox=""  />
                 </div>
             </div>
         </>
