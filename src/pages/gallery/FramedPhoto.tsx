251284e037b63a981cbbbcfafa7c982592032898
rikgj <44588337+rikgj@users.noreply.github.com>
2024-12-18 13:49:41 +0100

    fikser litt på rammen for å vise bildene bedre

--- src/pages/gallery/FramedPhoto.tsx
+++ src/pages/gallery/FramedPhoto.tsx
@@ -1,18 +1,35 @@
 import styles from "./Gallery.module.scss";
+import {useEffect, useState} from "react";
 
 
-type FramedPjotoProps = {
+type FramedPhotoProps = {
     srcDoc: string;
 }
 
-export function FramedPhoto(props: FramedPjotoProps) {
+export function FramedPhoto(props: FramedPhotoProps) {
+    const initialData = '';
+    const [data, setData,] = useState(initialData);
+    const getData = async (url: string) => {
+        const response = await fetch(url);
+        return await response.text();
+    }
+
+    useEffect(() => {
+        getData(props.srcDoc).then(data => {
+            console.log(data);
+            setData(data);
+        });
+    },[props.srcDoc])
+
+    if (data === initialData) {
+        return <div>Loading...</div>
+    }
     return (
         <>
             <div className={styles.frame}>
-                <div className={`${styles.framedPhotoInfo} ${styles.left} ${styles.code}`}>{props.srcDoc}</div>
-                <div className={`${styles.framedPhotoInfo} ${styles.right} ${styles.code}`}>{props.srcDoc}</div>
+                <div className={`${styles.framedPhotoInfo} ${styles.right} ${styles.code}`}>{data}</div>
                 <div className={styles.framedPhoto}>
-                    <iframe src={props.srcDoc} sandbox=""  />
+                    <iframe src={props.srcDoc} sandbox=""/>
                 </div>
             </div>
         </>
--- src/pages/gallery/Gallery.tsx
+++ src/pages/gallery/Gallery.tsx
@@ -2,47 +2,12 @@ import styles from "./Gallery.module.scss";
 import {FramedPhoto} from "./FramedPhoto.tsx";
 
 export const Gallery = () => {
-// take start ref as parameter
-    // must fetch css and js separately
-    // const url = "https://raw.githubusercontent.com/rikgj/journey-react/refs/heads/main/src/pages/Gallery.tsx"
-
-    // const [photos, setPhotos] = useState([""]);
-    // const [index, setIndex] = useState(1);
-// const viteFlag = "window.__vite_plugin_react_preamble_installed__ = true";
-    const urls = (Array.from({length: 4}).map((_, i) => `/photos/photo-${i+1}.html`));
-    // useEffect(() => {
-        // console.log(index);
-
-        // Promise.all(urls.map(url => fetch(url)))
-        //         .then(response => response.text())
-        //     .then((values) => {
-        //         setPhotos([...values.map(value => value.text())]);
-        //         console.log(values.map(value => value.text()));
-        //     })
-
-        // fetch(url)
-        //     .then(response => response.text())
-        //     .then(res => {
-        //         console.log(res);
-        //         console.log(photos);
-        //         if (res.length > 0 && !res.includes(viteFlag)) {
-        //             setIndex(prevIndex => prevIndex + 1);
-        //             setPhotos(prevState => {
-        //                 return [...prevState.filter(s => s.length > 0), res]
-        //             });
-        //         }else{
-        //             console.log("No more photos");
-        //         }
-        //     })
-    // }, [index]);
+    const urls = (Array.from({length: 8}).map((_, i) => `/photos/photo-${i+1}.html`));
 
     return (
         <>
             <h1>A Gallery!</h1>
             <div className={styles.galleryContainer}>
-                {/*{photos.map((photo, index) => {*/}
-                {/*    return <FramedPhoto key={index} srcDoc={photo}/>*/}
-                {/*})}*/}
                 {urls.map((url, index) => {
                     return <FramedPhoto key={index} srcDoc={url}/>
                 })}
