aac85fd57f34e424b9b63678c05ae37927a12c9a
rikgj <44588337+rikgj@users.noreply.github.com>
2024-12-21 01:10:34 +0100

    Lander på å beholde iframe for å lettere kunne vise ok html og css fra bildene på en grei måte.
    - Det tar tid å skrive om til react componenter med scss
    - Det er uleselig i json-format
    -- Det har liten hensikt å bruke json til å referer, men kan være jeg gjør det for å få en bedre loop?
    -Jeg vil ikke bruke innerHtml
    - FramedPhoto kan nå vise et bilde gitt at det er wrappet i en div, men får da problem med hvordan css-en skal håndteres på en grei måte uten at alt overskrives overalt

--- src/pages/gallery/FramedPhoto.tsx
+++ src/pages/gallery/FramedPhoto.tsx
@@ -1,9 +1,29 @@
 import styles from "./Gallery.module.scss";
 import {useEffect, useState} from "react";
+import html_beautify from "js-beautify";
 
 
 type FramedPhotoProps = {
     srcDoc: string;
+}
+const html_format_options ={
+    "indent_size": "4",
+    "indent_char": " ",
+    "max_preserve_newlines": "5",
+    "preserve_newlines": true,
+    "keep_array_indentation": false,
+    "break_chained_methods": false,
+    "indent_scripts": "normal",
+    "brace_style": "collapse",
+    "space_before_conditional": false,
+    "unescape_strings": false,
+    "jslint_happy": false,
+    "end_with_newline": false,
+    "wrap_line_length": "0",
+    "indent_inner_html": false,
+    "comma_first": false,
+    "e4x": false,
+    "indent_empty_lines": false
 }
 
 export function FramedPhoto(props: FramedPhotoProps) {
@@ -14,10 +34,17 @@ export function FramedPhoto(props: FramedPhotoProps) {
         return await response.text();
     }
 
-    useEffect(() => {
+        useEffect(() => {
         getData(props.srcDoc).then(data => {
-            console.log(data);
-            setData(data);
+            const bodyStart = data.indexOf('<body>');
+            const bodyEnd = data.indexOf('</body>');
+            const body = data.substring(bodyStart + 6, bodyEnd);
+            const styleStart = data.indexOf('<style>');
+            const styleEnd = data.indexOf('</style>');
+            const style = data.substring(styleStart + 7, styleEnd);
+            const scopedHtml = body + `<style>${style}</style>`;
+
+            setData(html_beautify.html(scopedHtml, html_format_options));
         });
     },[props.srcDoc])
 
